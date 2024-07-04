b337250c7eddacb210370a721f12b564f668d52c
Christopher Trent <ChristopherJTrent@outlook.com>
2024-07-04 15:24:47 -0700

    fix issue where entire app would move inside viewport when modal opened

--- src/Components/Layouts/App.tsx
+++ src/Components/Layouts/App.tsx
@@ -5,7 +5,7 @@ export type AppLayoutProps = {}
 
 export default function AppLayout({children}:PropsWithChildren<AppLayoutProps>) {
   return (
-    <Box width='100vw' height='100vh' backgroundColor='#282C37'>
+    <Box width='100vw' height='100vh' backgroundColor='#282C37' position='fixed' top='0px' left='0px'>
       <Box width='100vw' height='30px' className='titlebar' margin='0px'>
         <Flex align='center'>
           <Text marginLeft='1em' paddingTop='2px'>Magian Launcher</Text>
