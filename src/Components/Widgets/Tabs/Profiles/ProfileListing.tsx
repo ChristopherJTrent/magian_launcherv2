b9fc9ce021f006612928c257dc972e2c7b475541
Christopher Trent <ChristopherJTrent@outlook.com>
2024-07-04 17:16:03 -0700

    taking a break

--- src/Components/Widgets/Tabs/Profiles/ProfileListing.tsx
+++ src/Components/Widgets/Tabs/Profiles/ProfileListing.tsx
@@ -15,14 +15,12 @@ export default function ProfileListing() {
   //     return dispatch(receiveProfiles(v))
   //   }).catch(_ => {})
   // }, [dispatch])
-  return <>
-    <ul>
-      {Object.keys(profiles).map((v) => <li key={v}>
-        <ProfileElement profileName={v} />
-        </li>)}
-    </ul>
-    <Flex direction='row-reverse'>
+  return <Flex direction='column' justifyContent='space-between' height="calc(100vh - 30px)">
+      <ul>
+        {Object.keys(profiles).map((v) => <li key={v}>
+          <ProfileElement profileName={v} />
+          </li>)}
+      </ul>
       <NewProfileForm />
     </Flex>
-  </>
 }
