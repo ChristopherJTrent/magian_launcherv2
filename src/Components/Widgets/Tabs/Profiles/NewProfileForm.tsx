9abe327cf348821aa79d645d9e6c571599c4c850
Christopher Trent <ChristopherJTrent@outlook.com>
2024-07-09 13:36:42 -0700

    finish new profile form

--- src/Components/Widgets/Tabs/Profiles/NewProfileForm.tsx
+++ src/Components/Widgets/Tabs/Profiles/NewProfileForm.tsx
@@ -1,11 +1,36 @@
 import { AddIcon } from "@chakra-ui/icons"
-import { Box, Button, Drawer, DrawerCloseButton, DrawerContent, DrawerOverlay, Editable, EditableInput, EditablePreview, Flex, IconButton, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Text, useDisclosure } from "@chakra-ui/react"
+import { 
+  Button, 
+  Editable, 
+  EditableInput, 
+  EditablePreview, 
+  Flex, 
+  IconButton, 
+  Modal, 
+  ModalBody, 
+  ModalCloseButton, 
+  ModalContent, 
+  ModalFooter, 
+  ModalHeader, 
+  ModalOverlay, 
+  NumberDecrementStepper, 
+  NumberIncrementStepper, 
+  NumberInput, 
+  NumberInputField, 
+  NumberInputStepper, 
+  Switch, 
+  Text, 
+  useDisclosure 
+} from "@chakra-ui/react"
 import { useState } from "react"
-import CustomEditablePreview from "../../customEditablePreview"
 import EditableControls from "../../EditableControls"
+import Profile, { defaultAddons, defaultPlugins, defaultPolPlugins } from "../../../../lib/data/Profile"
+import { useAppDispatch } from "../../../../lib/store/store"
+import { receiveProfile } from "../../../../lib/store/ProfileReducer"
 
 export default function NewProfileForm() {
   const {isOpen, onOpen, onClose} = useDisclosure()
+  const dispatch = useAppDispatch()
 
   const [name, setName] = useState('New Profile')
   const [windowResolutionX, setWindowResolutionX] = useState(1920)
@@ -15,8 +40,19 @@ export default function NewProfileForm() {
   const [useDefaultAddons, setUseDefaultAddons] = useState(true)
   const [useDefaultPolPlugins, setUseDefaultPolPlugins] = useState(true)
 
-  const SaveOnClose = () => {
-
+  const saveProfile = () => {
+    const profile = new Profile(
+      name,
+      useDefaultAddons ? defaultAddons : [],
+      useDefaultPlugins ? defaultPlugins : [],
+      useDefaultPolPlugins ? defaultPolPlugins : [],
+      isPrivateServer
+    )
+    profile.settings.windowResolutionX = windowResolutionX
+    profile.settings.windowResolutionY = windowResolutionY
+    window.electron.ipcRenderer.saveProfile(profile).then(() => {
+      dispatch(receiveProfile(profile))
+    })
   }
 
   return <>
@@ -25,24 +61,16 @@ export default function NewProfileForm() {
     aria-label="New Profile"
     onClick={onOpen}
     _hover={{opacity: 1, transform: "scale(1)"}}/>
-    {/* <Drawer onClose={onClose} isOpen={isOpen} size="md">
-      <DrawerOverlay />
-      <DrawerContent>
-        <Flex direction='column'>
-          <DrawerCloseButton marginTop='30px'/>
-        </Flex>
-      </DrawerContent>
-    </Drawer> */}
     <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose} >
       <ModalOverlay />
       <ModalContent backgroundColor='#282C37'>
         <ModalHeader>Create New Profile</ModalHeader>
         <ModalCloseButton />
         <ModalBody padding='none' >
-          <Flex direction='column'>
-          <Text fontWeight='700'>
-            Profile Name
-          </Text>
+          <Flex direction='column' rowGap={'5px'}>
+            <Text fontWeight='700'>
+              Profile Name
+            </Text>
             <Editable defaultValue={name}
                 onSubmit={(nextValue) => setName(nextValue)}
                 padding='0px'
@@ -81,10 +109,39 @@ export default function NewProfileForm() {
                   <NumberDecrementStepper />
                 </NumberInputStepper>
             </NumberInput>
+            <Flex direction={"row"} justify={'space-between'}>
+              <Text fontWeight={700}>
+                Private Server?
+              </Text>
+              <Switch onChange={() => {setIsPrivateServer(!isPrivateServer)}} isChecked={isPrivateServer} />
+            </Flex>
+            <Flex direction={"row"} justify={'space-between'}>
+              <Text fontWeight={700}>
+                Use Default Playonline Plugins?
+              </Text>
+              <Switch onChange={() => {setUseDefaultPolPlugins(!useDefaultPolPlugins)}} isChecked={useDefaultPolPlugins}/>
+            </Flex>
+            <Flex direction={"row"} justify={'space-between'}>
+              <Text fontWeight={700}>
+                Use Default Plugins?
+              </Text>
+              <Switch onChange={() => {setUseDefaultPlugins(!useDefaultPlugins)}} isChecked={useDefaultPlugins}/>
+            </Flex>
+            <Flex direction={"row"} justify={'space-between'}>
+              <Text fontWeight={700}>
+                Use Default Addons?
+              </Text>
+              <Switch onChange={() => {setUseDefaultAddons(!useDefaultAddons)}} isChecked={useDefaultAddons}/>
+            </Flex>
           </Flex>
         </ModalBody>
         <ModalFooter>
-          <Button backgroundColor="#D35547">
+          <Button 
+              backgroundColor="#D35547"
+              onClick={() => {
+                saveProfile()
+                onClose()
+              }}>
             Save
           </Button>
           <Button onClick={onClose}>Cancel</Button>
