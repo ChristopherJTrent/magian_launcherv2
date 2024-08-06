4cc30d896aad4d1b059d8ca56b93fd5b90d27c49
Christopher Trent <ChristopherJTrent@outlook.com>
2024-08-06 13:57:59 -0700

    add repository download test code

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,31 @@
-import { app, BrowserWindow, ipcMain} from 'electron';
-import path from 'path';
-import { updateElectronApp } from 'update-electron-app';
-import registerIPCCallbacks from './ipcHandlers';
-import { downloadYamlFile } from '@lib/util/helpers/YAML/fileHandler';
+import { app, BrowserWindow, ipcMain} from 'electron'
+import path from 'path'
+import { updateElectronApp } from 'update-electron-app'
+import registerIPCCallbacks from './ipcHandlers'
+import doRepositoryUpdates, { installRemoteRepository } from '@lib/util/Installation/RepositoryInstaller'
+import { getInstalledRepositories } from 'Zod/installedRepositories'
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
-  app.quit();
+  app.quit()
 }
 
 registerIPCCallbacks(ipcMain)
 
 updateElectronApp()
 
-downloadYamlFile('gh:ChristopherJTrent/magian_launcherv2@master/exampleRepo.yaml').then((v) => {
-  console.log(v)
-})
+
+{
+  const remote = 'gh:ChristopherJTrent/magian_launcherv2@master/exampleRepo.yaml'
+  const repos = getInstalledRepositories()
+  if (repos.success && repos.data.some((v) => v.remote === remote )) {
+    console.log('file found, data read. repositories updating...')
+    doRepositoryUpdates()
+  } else {
+    console.log(repos.error)
+    installRemoteRepository(remote)
+  }
+}
 
 const createWindow = () => {
   const RESOURCES_PATH = app.isPackaged
@@ -43,27 +53,27 @@ const getAssetPath = (...paths: string[]): string => {
     },
     resizable: false,
     maximizable: false
-  });
+  })
 
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`))
   }
 
   // Open the DevTools.
   if(!app.isPackaged) {
-    mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools()
   }
-};
+}
 
 
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 if (!app.isPackaged) {
   // This is broken and terrible, but it works.
@@ -87,17 +97,17 @@ if (!app.isPackaged) {
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createWindow()
   }
-});
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
