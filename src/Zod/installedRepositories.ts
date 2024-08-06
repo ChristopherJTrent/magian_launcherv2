df3483b50486598038579f88df4953b38c279c81
Christopher Trent <ChristopherJTrent@outlook.com>
2024-08-06 15:02:56 -0700

    add code to manage repositories.json

--- src/Zod/installedRepositories.ts
+++ src/Zod/installedRepositories.ts
@@ -1,10 +1,35 @@
-import { z } from "zod";
+import { SafeParseReturnType, z } from "zod"
 import validateSemver from 'semver/functions/valid'
-import { repoRegex } from "./Repository";
+import { repoRegex } from "./Repository"
+import { readFileSync, writeFileSync } from "fs"
+import { join } from "path"
+import { INSTALL_LOCATION } from "@lib/util/Installation/paths"
 
 const installedRepositories = z.array(z.object({
     installedVersion: z.string().refine(validateSemver),
     remote: z.string().regex(repoRegex)
 }))
 
+export function getInstalledRepositories() {
+    try{
+        return installedRepositories.safeParse(JSON.parse(
+            readFileSync(
+                join(INSTALL_LOCATION, 'repositories.json')
+            ).toString()
+        ))
+    } catch (err) {
+        console.log(err)
+        return {success: false, error: err} as SafeParseReturnType<z.infer<typeof installedRepositories>, z.infer<typeof installedRepositories>>
+    }
+}
+
+export function addInstalledRepository(version: string, location: string) {
+    const installed = installedRepositories.safeParse([{installedVersion:version, remote: location}])
+    const existing = getInstalledRepositories()
+    if(!installed.success || !existing.success) return
+    writeFileSync(
+        join(INSTALL_LOCATION, 'repositories.json'),
+        JSON.stringify([...existing.data, ...installed.data])
+    )
+}
 export default installedRepositories
