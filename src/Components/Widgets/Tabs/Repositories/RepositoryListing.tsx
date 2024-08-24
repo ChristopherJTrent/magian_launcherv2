5f24d90a4fe9f4efc1900bf430f4e5d924573a69
Christopher Trent <ChristopherJTrent@outlook.com>
2024-08-23 21:07:41 -0700

    Create listing of repositories

--- src/Components/Widgets/Tabs/Repositories/RepositoryListing.tsx
+++ src/Components/Widgets/Tabs/Repositories/RepositoryListing.tsx
@@ -1,5 +1,22 @@
+import { receiveRepositories, repositoryMapping } from "@lib/store/repositoriesReducer"
+import { useAppDispatch, useAppSelector } from "@lib/store/store"
+import RepositoryElement from "./RepositoryElement"
+import { useEffect } from "react"
+import { List } from "@chakra-ui/react"
 
 
 export default function RepositoryListing() {
-    return <></>
+    const repositories: repositoryMapping | undefined = useAppSelector((state) => state.repositories)
+    const dispatch = useAppDispatch()
+    useEffect(() => {
+        window.electron.ipcRenderer.getInstalledRepositories().then(v => {
+            dispatch(receiveRepositories(v))
+        })
+    })
+    return <>
+        {
+            repositories && repositories.map((v) => <List>{<RepositoryElement repository={v} />}</List>)
+                || <> </>
+        }
+    </>
 }
